33c6dcb5b66cb0c2c2b4441ad96a68dcb1574ce2
alamhossainhr <mhalam670@gmail.com>
2026-09-06 13:28:45 +0600

    Use uv pip install to comply with managed python environment

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Build the project
-echo "Building the project..."
-python3 -m pip install -r requirements.txt --no-cache-dir --break-system-packages
+echo "Installing dependencies with uv..."
+uv pip install -r requirements.txt --system
 
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
