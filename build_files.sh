5607ab11d49f4e90375aadaf57798dd249187345
alamhossainhr <mhalam670@gmail.com>
2026-09-06 13:26:43 +0600

    Add --break-system-packages flag for Vercel Python build

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
+# Build the project
 echo "Building the project..."
-python3 -m pip install -r requirements.txt --no-cache-dir
+python3 -m pip install -r requirements.txt --no-cache-dir --break-system-packages
 
 echo "Collect Static..."
 python3 manage.py collectstatic --noinput --clear
